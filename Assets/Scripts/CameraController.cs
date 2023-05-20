0fcd5fe7fe18ae13bc350760b6b54661b22e956e
SonOfIgneel <menonarjun98@gmail.com>
2023-05-20 11:40:58 +0530

    player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController player;
+    private float sensitivity = 200f;
+    private float clampangle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
+
+    public void Look()
+    {
+        float mouseVerical = -Input.GetAxis("Mouse Y");
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+
+        this.verticalRotation += mouseVerical * this.sensitivity * Time.deltaTime;
+        this.horizontalRotation += mouseHorizontal * this.sensitivity * Time.deltaTime;
+
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -this.clampangle, this.clampangle);
+
+        this.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0f, 0f);
+        this.player.transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
+    }
+}
